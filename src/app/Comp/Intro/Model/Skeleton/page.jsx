a32b540907c4cda428a69031e7db28297df4d4d6
hamadhussain <108784635+hamadhussain@users.noreply.github.com>
2024-08-17 16:41:08 +0500

    ldddd

--- src/app/Comp/Intro/Model/Skeleton/page.jsx
+++ src/app/Comp/Intro/Model/Skeleton/page.jsx
@@ -87,21 +87,58 @@ import React, { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
-export default function Model(props) {
+// export default function Model() {
+//   const { nodes, materials } = useGLTF("/skull_salazar_downloadable.glb");
+  
+//   const modelRef = useRef();
+//   // useEffect(() => {
+
+//   useFrame(() => {
+//     if (modelRef.current) {
+//       modelRef.current.rotation.y += 0.007;
+//     }
+//   });
+// // }, []);
+
+//   return (
+//     <group   ref={modelRef}>
+//       <group rotation={[-Math.PI / 2, 0.019, 0]} scale={[3, 3, 3]}>
+//         <group rotation={[2.2, 0, 0]}>
+//           <mesh
+//             castShadow
+//             receiveShadow
+//             geometry={nodes.defaultMaterial.geometry}
+//             material={materials.Rosa_material}
+//           />
+//           <mesh
+//             castShadow
+//             receiveShadow
+//             geometry={nodes.defaultMaterial_1.geometry}
+//             material={materials.defaultMat_material}
+//           />
+//         </group>
+//       </group>
+//     </group>
+//   );
+// }
+
+// useGLTF.preload("/skull_salazar_downloadable.glb");
+
+const Model = () => {
   const { nodes, materials } = useGLTF("/skull_salazar_downloadable.glb");
   
   const modelRef = useRef();
   // useEffect(() => {
 
-  // useFrame(() => {
-  //   if (modelRef.current) {
-  //     modelRef.current.rotation.y += 0.007;
-  //   }
-  // });
+  useFrame(() => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += 0.007;
+    }
+  });
 // }, []);
 
   return (
-    <group {...props} dispose={null} ref={modelRef}>
+    <group   ref={modelRef}>
       <group rotation={[-Math.PI / 2, 0.019, 0]} scale={[3, 3, 3]}>
         <group rotation={[2.2, 0, 0]}>
           <mesh
@@ -122,4 +159,4 @@ export default function Model(props) {
   );
 }
 
-// useGLTF.preload("/skull_salazar_downloadable.glb");
+export default Model
--- src/app/loading.js
+++ src/app/loading.js
@@ -1,6 +1,9 @@
+import Image from "next/image"
+
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
     return <>
-    Loading
+    <div className="h-screen d flex justify-center items-center">
+      <Image src="/loader.gif" width={200} height={100}/>
+    </div>
     </>
   }
