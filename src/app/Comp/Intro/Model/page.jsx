412aa16b96d210b5a8ab5fae0469e1e78e580c8b
hamadhussain <108784635+hamadhussain@users.noreply.github.com>
2024-08-19 10:24:47 +0500

    nj

--- src/app/Comp/Intro/Model/page.jsx
+++ src/app/Comp/Intro/Model/page.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
-// import Model from "./Skeleton/page";
+import Model from "./Skeleton/page";
 
 const Three = () => {
   return (
     <Canvas>
       {/* <ambientLight intensity={112}/> */}
       <directionalLight position={[0, 2, 1]} intensity={4} />
-      {/* <Model /> */}
+      <Model />
     </Canvas>
   );
 };
